d80a86dbd833ef6dea2be7fe3b4f71e6d526b279
Purna Boyapati <bpcrao@gmail.com>
2013-06-05 06:36:30 +0530

    changes

--- grails-app/domain/com/grailsinaction/User.groovy
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -1,13 +1,18 @@
 package com.grailsinaction
 
 class User {
-    String userId
-    String password
-    String homepage
-    Date dateCreated
-
-    static constraints = {
-        userId (size:3..20,unique:true)
-        password(size: 5..10)
-    }
+	String userId
+	String password
+	String homepage
+	Date dateCreated
+	Profile profile
+	static constraints = {
+		userId (size:3..20,unique:true)
+		password(size: 5..10,validator: { passwd, user ->
+			return passwd != user.userId
+		})
+		profile(nullable: true)
+	}
+		static mapping = { profile lazy:false }
+		static hasMany = [ posts : Post, tags : Tag,following : User ]
 }
