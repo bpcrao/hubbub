8f164c6e3891e16a81d86dbe6018498071389aa8
Purna <purna.boyapati@imaginea.com>
2013-06-11 11:19:55 +0530

    commitrf

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,8 @@
 // Place your Spring DSL code here
+import org.apache.activemq.spring.ActiveMQConnectionFactory
+
 beans = {
+    connectionFactory(ActiveMQConnectionFactory) {
+        brokerURL = "tcp://localhost:61616"
+    }
 }
